e6617ec6c83c5856d06444c27d979d6271b3ef35
Robrecht Siera <robrecht.siera@holoncom.eu>
2015-03-17 12:20:00 +0100

    Trying to Build Extract the zip to C:\Program Files (x86). Run the bat file (maybe change the params inside)

---
+++ src/Build.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\nant-0.92\bin\Nant.exe" -buildfile:BugNET.build -D:teamcity.build.checkoutDir=".\build\checkout" -D:build.dir=".\build\result" -D:build.number="1.0.0.0"
+pause 
